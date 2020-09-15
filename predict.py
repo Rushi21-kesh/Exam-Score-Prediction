4999a71e9835cf593df20580401b70c243dcdf49
Rushikesh Lavate <61430438+Rushi21-kesh@users.noreply.github.com>
2020-09-15 15:38:12 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import request
 import plotly.express as px
 from sklearn.model_selection import train_test_split  
 from sklearn.linear_model import LinearRegression  
